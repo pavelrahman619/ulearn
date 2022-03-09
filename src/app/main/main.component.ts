fc4b48d798bcee9d1e71fcba3ea2b340c80e0aa7
pavelrahman619 <96363207+pavelrahman619@users.noreply.github.com>
2022-03-09 16:17:19 +0600

    fixed bubble

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -3,17 +3,37 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  styleUrls: ['./main.component.scss'],
 })
 export class MainComponent implements OnInit {
+  menuButtons = [
+    {
+      menuName: 'Settlement',
+      menuList: ['Adjumani', 'BidiBidi'],
+    },
+    {
+      menuName: 'Geographic Scope',
+      menuList: ['Global/multi-country/foreign', 'National'],
+    },
+    {
+      menuName: 'Type of Organization',
+      menuList: ['Refugee-led organization', 'Community-based organization'],
+    },
+    {
+      menuName: 'Thematic Areas of Work',
+      menuList: ['Basic Needs', 'Camp Coordination and Management'],
+    },
+    {
+      menuName: 'Target Demographic',
+      menuList: ['Women', 'Children(< 18 Years)'],
+    },
+  ];
 
   numSequence(n: number): Array<number> {
     return Array(n);
   }
-  
-  constructor() { }
 
-  ngOnInit() {
-  }
+  constructor() {}
 
+  ngOnInit() {}
 }
